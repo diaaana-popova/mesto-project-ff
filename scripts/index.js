aace8c52a5e1c02e76d8c9e347b41875b0a95ba9
Diana Popova <diaaana.popova@gmail.com>
2025-04-08 20:30:25 +0300

    Fix mistakes

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,25 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const placesItem = document.querySelector('.places__list');
 
-function cardAppend(element, callback) {
+function createCard(element, callback) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
   cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.name;
   cardElement.querySelector('.card__title').textContent = element.name;
+
   const deleteButton = cardElement.querySelector('.card__delete-button');
 
-  placesItem.append(cardElement);
-
   deleteButton.addEventListener('click', callback);
+
+  return cardElement;
 };
 
 function removeCard(event) {
-  event.target.parentElement.remove();
+  event.target.closest('.places__item').remove();
 }
 
 initialCards.forEach((cardData) => {
-  cardAppend(cardData, removeCard);
+  const card = createCard(cardData, removeCard);
+  placesItem.append(card);
 });
